1a6d0ee79908572914aa936dfc7209cf9deb3db6
icd107 <icd107@live.com>
2023-11-20 18:25:28 -0800

    Midterm demo complete

--- Assets/Scripts/GestureDetection1.cs
+++ Assets/Scripts/GestureDetection1.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Oculus.Interaction.Samples
 {
@@ -10,9 +11,8 @@ namespace Oculus.Interaction.Samples
         [SerializeField] private ActiveStateSelector thumbsUpR;
         [SerializeField] private ActiveStateSelector thumbsDownL;
         [SerializeField] private ActiveStateSelector thumbsDownR;
-        [SerializeField] private ActiveStateSelector stopL;
-        [SerializeField] private ActiveStateSelector stopR;
-        [SerializeField] private ActiveStateSelector scissor;
+        [SerializeField] private ActiveStateSelector rockR;
+        [SerializeField] private ActiveStateSelector rockL;
 
         public GameObject lineDetector;
         public GameObject scoreBoard;
@@ -21,9 +21,14 @@ namespace Oculus.Interaction.Samples
         private bool isThumbsUpR = false;
         private bool isThumbsDownL = false;
         private bool isThumbsDownR = false;
-        private bool isStopL = false;
-        private bool isStopR = false;
-        private bool isScissor = false;
+        private bool isRockR = false;
+        private bool isRockL = false;
+
+        private bool isThumbsUpBefore = false;
+
+        public bool isLineOn;
+        private int leftScore;
+        private int rightScore;
 
         // Start is called before the first frame update
         void Start()
@@ -36,38 +41,60 @@ namespace Oculus.Interaction.Samples
             thumbsDownL.WhenUnselected += () => { isThumbsDownL = false; };
             thumbsDownR.WhenSelected += () => { isThumbsDownR = true; };
             thumbsDownR.WhenUnselected += () => { isThumbsDownR = false; };
-            stopL.WhenSelected += () => { isStopL = true; };
-            stopL.WhenUnselected += () => { isStopL = false; };
-            stopR.WhenSelected += () => { isStopR = true; };
-            stopR.WhenUnselected += () => { isStopR = false; };
-            scissor.WhenSelected += () => { isScissor = true; };
-            scissor.WhenUnselected += () => { isScissor = false; };
+            rockR.WhenSelected += () => { isRockR = true; };
+            rockR.WhenUnselected += () => { isRockR = false; };
+            rockL.WhenSelected += () => { isRockL = true; };
+            rockL.WhenUnselected += () => { isRockL = false; };
 
+            scoreBoard.GetComponent<CanvasGroup>().alpha = 0.0f;
+            isLineOn = false;
+            leftScore = 0;
+            rightScore = 0;
+
+            string scoreText = "Scoreboard!\nPlayer 1: " + leftScore + "\nPlayer 2: " + rightScore;
+            scoreBoard.GetComponent<Text>().text = scoreText;
         }
 
         // Update is called once per frame
         void Update()
         {
             // Turn on the detection
-            if (isThumbsUpL || isThumbsUpR)
-            {
-                lineDetector.SetActive(true);
-            }
-
-            // Pull up score board
             if (isThumbsDownL || isThumbsDownR)
             {
-                scoreBoard.SetActive(true);
+                lineDetector.SetActive(true);
+                isLineOn = true;
+                isThumbsUpBefore = false;
+            }
+
+            // Pull up score board and update score
+            if ((isThumbsUpL || isThumbsUpR) && !isThumbsUpBefore)
+            {
+                scoreBoard.GetComponent<CanvasGroup>().alpha = 1.0f;
+
+                // Update score
+                if(isThumbsUpL) {
+                    leftScore += 1;
+                }
+                else if(isThumbsUpR) { 
+                    rightScore += 1; 
+                }
+
+                string scoreText = "Scoreboard!\nLeft Player: " + leftScore + "\nRight Player: " + rightScore;
+
+                scoreBoard.GetComponent<Text>().text = scoreText;
+
+                isThumbsUpBefore = true;
             }
 
             // Stop detection and score board
-            //if (isStopL || isStopR)
-            if (isScissor)
+            if (isRockR || isRockL)
             {
-                if (lineDetector) { lineDetector.SetActive(false); }
-                if (scoreBoard) { scoreBoard.SetActive(false); }
-
+                isLineOn = false;
+                scoreBoard.GetComponent<CanvasGroup>().alpha = 0.0f;
+                isThumbsUpBefore = false;
             }
+
+            //make sure if stop is shown alongside thumbsup/thumbsdown
         }
     }
 }
--- Assets/Scripts/LineUpManager.cs
+++ Assets/Scripts/LineUpManager.cs
@@ -2,48 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LineUpManager : MonoBehaviour
+namespace Oculus.Interaction.Samples
 {
-    public GameObject headSet;
-    public GameObject handR;
-    public GameObject handL;
-
-    Vector3 headPosition;
-    Vector3 leftPosition;
-
-    LineRenderer lineRenderer;
-
-    // Start is called before the first frame update
-    void Start()
+    public class LineUpManager : MonoBehaviour
     {
-        //For creating line renderer object
-        lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
-        lineRenderer.startColor = Color.black;
-        lineRenderer.endColor = Color.black;
-        lineRenderer.startWidth = 0.01f;
-        lineRenderer.endWidth = 0.01f;
-        lineRenderer.positionCount = 2;
-        lineRenderer.useWorldSpace = true;
-        updatePositions();
-    }
+        public GameObject headSet;
+        public GameObject handR;
+        public GameObject handL;
+        public GameObject gestureDetector;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(handL.activeInHierarchy && !OVRInput.IsControllerConnected(OVRInput.Controller.Hands))
+        Vector3 headPosition;
+        Vector3 leftPosition;
+        Vector3 rightPosition;
+
+        LineRenderer lineRenderer;
+
+        public bool isLeftFurther;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //For creating line renderer object
+            lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
+            lineRenderer.startColor = Color.black;
+            lineRenderer.endColor = Color.black;
+            lineRenderer.startWidth = 0.01f;
+            lineRenderer.endWidth = 0.01f;
+            lineRenderer.positionCount = 2;
+            lineRenderer.useWorldSpace = true;
+            updatePositions();
+            isLeftFurther = false;
+        }
+
+        // Update is called once per frame
+        void Update()
         {
             updatePositions();
-        }
-        headPosition = headSet.transform.position;
-        float distance = Vector3.Distance(headPosition, leftPosition);
-                        
-        //For drawing line in the world space, provide the x,y,z values
-        lineRenderer.SetPosition(0, headPosition); //x,y and z position of the starting point of the line
-        lineRenderer.SetPosition(1, leftPosition); //x,y and z position of the end point of the line
-    }
+            
+            float distanceL = Vector3.Distance(headPosition, leftPosition);
+            float distanceR = Vector3.Distance(headPosition, rightPosition);
 
-    void updatePositions()
-    {
-        leftPosition = handL.transform.position;
+            Vector3 furtherTarget = new Vector3();
+
+            if (distanceL > distanceR) { 
+                furtherTarget = leftPosition;
+                isLeftFurther = true;
+            }
+            else { 
+                furtherTarget = rightPosition; 
+                isLeftFurther = false; 
+            }
+
+            if(gestureDetector.GetComponent<GestureDetection1>().isLineOn)
+            {
+                lineRenderer.SetPosition(0, headPosition);
+                lineRenderer.SetPosition(1, furtherTarget);
+            }
+            else
+            {
+                //For drawing line in the world space, provide the x,y,z values
+                lineRenderer.SetPosition(0, headPosition); //x,y and z position of the starting point of the line
+                lineRenderer.SetPosition(1, headPosition); //x,y and z position of the end point of the line
+            }
+        }
+
+        void updatePositions()
+        {
+            headPosition = headSet.transform.position;
+
+            if (handL.transform.position != null && handL.transform.position != new Vector3(0,0,0))
+            { 
+                leftPosition = handL.transform.position;
+            }
+            if (handR.transform.position != null && handR.transform.position != new Vector3(0,0,0))
+            {
+                rightPosition = handR.transform.position;
+            } 
+        }
     }
 }
